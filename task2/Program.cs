474cc1469e3d00a4d4ccb87fcc83834382fecc75
Nikita <nikitya39@gmail.com>
2022-03-31 14:06:12 +0200

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = new int[5];
+for (int i = 0; i < array.Length; i++)
+{
+   array[i] = new Random().Next(1,10);
+   Console.WriteLine(array[i]);
+}  
+
+int sum = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0 )
+    {
+        sum += array [i];
+    }
+}
+Console.WriteLine();
+Console.WriteLine(sum);
