07e4897b4db8c0f6b660b301133edd60d2cb04b0
Nikita <nikitya39@gmail.com>
2022-03-31 14:48:16 +0200

    task33

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,12 +1,12 @@
-﻿double[] array = new double[10];
+﻿int[] array = new int[10];
 for (int i = 0; i < array.Length; i++)
 {
 array[i] = new Random().Next(100,1000);
 Console.WriteLine(array[i]);
 }
 
-double max = array [0];
-double min = array [0];
+int max = array [0];
+int min = array [0];
 
 for (int i = 0; i < array.Length; i++)
 {
