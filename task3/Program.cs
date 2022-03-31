8c800f11a25afa52c3b7d796607bd81fe6f229b8
Nikita <nikitya39@gmail.com>
2022-03-31 14:46:31 +0200

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,28 @@
+﻿double[] array = new double[10];
+for (int i = 0; i < array.Length; i++)
+{
+array[i] = new Random().Next(100,1000);
+Console.WriteLine(array[i]);
+}
+
+double max = array [0];
+double min = array [0];
+
+for (int i = 0; i < array.Length; i++)
+{
+if (array[i] > max)
+{
+max = array[i];
+}
+else if (array[i] < min)
+{
+min = array[i];
+}
+
+}
+
+double result = max - min;
+Console.WriteLine();
+Console.WriteLine($"максмальное значение массива {max}");
+Console.WriteLine($"минимальное значение массива {min}");
+Console.WriteLine($"разница {result}");
